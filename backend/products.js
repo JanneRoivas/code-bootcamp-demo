6de286c2dfc09abcb5cf813dce09b1685d5e3621
JanneRoivas <vihtorikallevihtori@gmail.com>
2020-04-07 11:52:47 +0300

    Dockerfilet

--- backend/products.js
+++ backend/products.js
@@ -1,10 +1,10 @@
 const pgp = require('pg-promise')()
 const db = pgp({
-    host: 'localhost',
-    port: 5432,
-    database: 'demo',
-    user: 'postgres',
-    password: 'postgres'
+    host: process.env.POSTGRES_HOST || "localhost",
+    port: process.env.POSTGRES_PORT || 5432,
+    database: process.env.POSTGRES_DB || "demo",
+    user: process.env.POSTGRES_USER || "postgres",
+    password: process.env.POSTGRES_PASSWORD || "postgres",
 })
 const getProducts = (req,res) => {
     const tyyppi = req.params.tyyppi
